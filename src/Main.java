da91e873b29a8408c0e562a981181de055d66002
AleksandrBobrovskikh <bobrosan@gmail.com>
2024-11-18 17:07:36 +0300

    Создание счетчика строк длиной 3 и менее символов.

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] data = {"1234", "1567", "computer science", "-2", "."};
+        int count = 0;
+        for (String s : data) {
+            if (s.length() < 4) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
